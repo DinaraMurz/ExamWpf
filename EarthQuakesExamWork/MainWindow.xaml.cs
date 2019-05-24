77d845609450c724a43558e2ed61f2499791f475
Доллар Музыка <dollarmudic@gmail.com>
2019-05-24 18:18:06 +0600

    Добавьте файлы проекта.

---
+++ EarthQuakesExamWork/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace EarthQuakesExamWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
+        //private List<Earthquake> _earthquakeList = new List<Earthquake>();
+        private Rootobject earthquake = new Rootobject();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string json = "";
+
+            WebClient client = new WebClient();
+            using (Stream stream = client.OpenRead(_url))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    json += reader.ReadToEnd();
+                }
+            }
+
+            earthquake = Newtonsoft.Json.JsonConvert.DeserializeObject<Rootobject>(json);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //{
+            //    new EarthQuake { Magnitude = "M", EpicenterDepth = "ED", Place = "P", Date = DateTime.Now }
+            //};
+            //dataGrid.Items.Add(earthQuakesList);
+
+            
+
+
+
+            //_RootobjectList = JsonConvert.DeserializeObject<List<Rootobject>>(json);
+            
+        }
+    }
+}
